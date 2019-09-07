f18d2d6b9fe352ea29257df8f36486769d9dc4f0
Mike Hayes <fikes428@gmail.com>
2019-09-07 17:41:45 -0400

    deleted duplicate code

--- liri.js
+++ liri.js
@@ -39,48 +39,6 @@ function switchCase() {
   }
 };
 
-
-require("dotenv").config();
-var fs = require("fs");
-var keys = require("./keys.js");
-var request = require('request');
-var Spotify = require('node-spotify-api');
-var spotify = new Spotify(keys.spotify);
-
-
-var action = process.argv[2];
-var parameter = process.argv[3];
-
-
-
-
-function switchCase() {
-
-  switch (action) {
-
-    case 'concert-this':
-      bandsInTown(parameter);                   
-      break;                          
-
-    case 'spotify-this-song':
-      spotSong(parameter);
-      break;
-
-    case 'movie-this':
-      movieInfo(parameter);
-      break;
-
-    case 'do-what-it-says':
-      getRandom();
-      break;
-
-      default:                            
-      logIt("Invalid Instruction");
-      break;
-
-  }
-};
-
 function bandsInTown(parameter){
 
 if (action === 'concert-this')
